c201a00affd56929b11bdf8e251bf64aeb7ea3d4
tdj <tdj@pro-shine.cn>
2020-03-06 10:00:06 +0800

    lambda 更新

--- src/main/java/lambda/LambdaTest.java
+++ src/main/java/lambda/LambdaTest.java
@@ -1,7 +1,7 @@
 package lambda;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executor;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -13,18 +13,21 @@ public class LambdaTest {
         };
         consumer.doSomething("123");
         doSomething(s -> System.out.println(s), "123");
-
-
-
+        List<Person> ps = new ArrayList<Person>();
+        ps.add(new Person("A", 12));
+        ps.add(new Person("B", 23));
+        ps.add(new Person("C", 34));
+        ps.add(new Person("D", 45));
+        checkAndExec(ps, p -> p.getName().startsWith("A"), p -> System.out.println(p.getAge()));
     }
 
     // 接口实现作为参数
-    public static void doSomething(LambdaInterface l, String s){
+    public static void doSomething(LambdaInterface l, String s) {
         l.doSomething(s);
     }
 
     // 内置的模板接口 可以减少函数式接口
-    public static void checkAndExec(List<Person> persons, Predicate<Person> predicate, Consumer<Person> consumer){
+    public static void checkAndExec(List<Person> persons, Predicate<Person> predicate, Consumer<Person> consumer) {
         for (Person localPerson : persons) {
             if (predicate.test(localPerson))
                 consumer.accept(localPerson);
